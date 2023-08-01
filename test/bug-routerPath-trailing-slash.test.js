0f11e552a27d75a78661424e3a7a98d3372523b2
unknown <yeeao@uci.edu>
2023-08-01 10:51:43 -0700

    changed test file typing

---
+++ test/bug-routerPath-trailing-slash.test.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const t = require('tap')
+const fastify = require('../fastify')()
+
+t.test('routerPath should not return url with a trailing slash', t => {
+  t.plan(1)
+
+  fastify.addHook('onRoute', ({ method, url }) => {
+    console.log('New route added:', method, url)
+  })
+
+  fastify.register(async (fastify) => {
+    fastify.get('/', async (request) => {
+      console.log(request.routerPath)
+    })
+  }, { prefix: '/foo' })
+
+  fastify.ready(() => {
+    fastify.inject({ method: 'GET', url: '/foo' })
+    fastify.inject({ method: 'GET', url: '/foo/' })
+  })
+
+  t.pass() // reaching here means previous lines run without error
+})
+
+// Mar 1 comment
+t.test('routerPath should not return url with a trailing slash2', t => {
+  t.plan(1)
+
+  fastify.addHook('onRoute', ({ method, url }) => {
+    console.log('New route added:', method, url)
+  })
+
+  fastify.register(async (fastify) => {
+    fastify.get('/', async (request) => {
+      console.log(request.routerPath)
+    })
+    fastify.get('/bar/', async (request) => {
+      console.log(request.routerPath)
+    })
+  }, { prefix: '/foo' })
+
+  fastify.ready(() => {
+    fastify.inject({ method: 'GET', url: '/foo' })
+    fastify.inject({ method: 'GET', url: '/foo/' })
+    fastify.inject({ method: 'GET', url: '/foo/bar/' })
+  })
+
+  t.pass() // reaching here means previous lines run without error
+})
